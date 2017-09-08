e1bafd1856809cd054bea3ff605c5f1049f7d99b
deebnntt <daniellebennettchan@gmail.com>
2017-09-08 11:13:17 -0400

    mock challenge

--- author.rb
+++ author.rb
@@ -1,1 +1,33 @@
+class Author
 
+  attr_accessor :name, :books
+
+
+  def initialize(name)
+    @name = name
+    @books = []
+  end
+
+  def books
+    @books
+  end
+
+  def write_book(title, wordcount)
+    new_book = Book.new(title, self, wordcount)
+    @books << new_book
+    book.author = self
+  end
+
+  def total_words
+    @books.map do |book|
+      book.wordcount
+    end.inject(0){|sum,x| sum + x }
+  end
+
+  def self.most_words
+    @books.author.max_by do |book|
+      book.total_words
+    end
+  end
+
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,18 @@
+class Book
 
+  attr_accessor :title, :author, :wordcount
+
+ALL = []
+
+  def initialize(title, author, wordcount)
+    @title = title
+    @author = author
+    @wordcount = wordcount
+    ALL << self
+  end
+
+  def self.all
+    ALL
+  end
+
+end
